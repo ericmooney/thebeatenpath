f6b0ada49eef5c5241cd5de5bb9fb01516661a25
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-17 10:13:36 -0700

    New folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebeatenpath::Application.config.secret_token = '625452fce456b2e26af10f76afa152290c2de4a684fe4a54e6bac737a505298d7cce7eba3094eb505e4dc4fd31d8c66678f72ed725f5a38ad7e17beec06a752f'
