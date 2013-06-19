175330615d31f2ebf18fbdd05d17aabc9c52606d
tomgalpin <tomgalpin@gmail.com>
2013-06-18 17:18:24 -0700

    For Sumeet

--- Gemfile
+++ Gemfile
@@ -37,3 +37,5 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use debugger
 # gem 'debugger'
+
+gem 'gmaps4rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require_dir ./gmaps4rails
 //= require_tree .
+
 
 $(function(){ $(document).foundation(); });
