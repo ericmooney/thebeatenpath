5b76652aadb2c7a8e669d06ef79ca1a9d3f1aa0b
tomgalpin <tomgalpin@gmail.com>
2013-06-21 18:55:38 -0700

    getting ready to merge

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -2,14 +2,15 @@ var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
 
+
 function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
   var mapOptions = {
     zoom:7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: chicago
-  }
+    mapTypeId: google.maps.MapTypeId.ROADMAP, //here is where we would add different types of maps
+  };
+
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   directionsDisplay.setMap(map);
 }
@@ -27,6 +28,18 @@ function calcRoute() {
       directionsDisplay.setDirections(response);
     }
   });
+
+var yelpMarker = document.getElementById('marker').value;
+
+var marker = new google.maps.Marker({
+  position: yelpMarker,
+  map: map,
+  });
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(function() {
+  calcRoute();
+});
