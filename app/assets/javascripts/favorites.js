45c9c0a7de73a932554476876bd4d1851442534f
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-21 18:56:02 -0700

    Add map with waypoints representing the yelp queries results

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,3 +1,4 @@
+
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
@@ -6,7 +7,7 @@ function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
   var mapOptions = {
-    zoom:7,
+    zoom: 6,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     center: chicago
   }
@@ -17,16 +18,54 @@ function initialize() {
 function calcRoute() {
   var start = document.getElementById('start').value;
   var end = document.getElementById('end').value;
+  var waypts = [];
+
+  var waypoints_hidden_inputs = $('.waypoints');
+  for (var i = 0; i < waypoints_hidden_inputs.length; i++) {
+    waypts.push({
+      location:waypoints_hidden_inputs[i].value,
+      stopover:true
+    });
+  }
+  console.log(waypts);
+
+
+  // var checkboxArray = document.getElementById('waypoints');
+  // for (var i = 0; i < checkboxArray.length; i++) {
+  //   if (checkboxArray.options[i].selected == true) {
+  //     waypts.push({
+  //         location:checkboxArray[i].value,
+  //         stopover:true});
+  //   }
+  // }
+
   var request = {
-      origin:start,
-      destination:end,
-      travelMode: google.maps.DirectionsTravelMode.DRIVING
+      origin: start,
+      destination: end,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING
   };
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById('directions_panel');
+      summaryPanel.innerHTML = '';
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i + 1;
+        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+      }
     }
   });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(function() {
+  calcRoute();
+})
