33763834083dc83e617c6ae45a72919e26b37993
Sumeet Jain <sumeet@sumeetjain.com>
2013-06-23 23:00:19 -0700

    Fix asset errors during deployment

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Thebeatenpath
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # This setting might fix errors during deployment.
+    config.assets.initialize_on_precompile = false
   end
 end
